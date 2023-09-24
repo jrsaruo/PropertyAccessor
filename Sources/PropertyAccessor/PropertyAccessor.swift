b70bad5ee57267e4d91f69aa0fbfb09a75cbc6a7
Yusaku Nishi <jrsaruo@gmail.com>
2023-09-24 17:03:12 +0900

    add: Accessor macro

--- Sources/PropertyAccessor/PropertyAccessor.swift
+++ Sources/PropertyAccessor/PropertyAccessor.swift
@@ -9,3 +9,6 @@
 /// produces a tuple `(x + y, "x + y")`.
 @freestanding(expression)
 public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "PropertyAccessorMacros", type: "StringifyMacro")
+
+@attached(accessor)
+public macro Accessor() = #externalMacro(module: "PropertyAccessorMacros", type: "PropertyAccessorMacro")
