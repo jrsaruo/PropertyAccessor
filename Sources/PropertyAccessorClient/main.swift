8696ad674d00e4bfa1e64374641877fca5cd12c7
Yusaku Nishi <jrsaruo@gmail.com>
2023-09-24 17:43:59 +0900

    add: samples

--- Sources/PropertyAccessorClient/main.swift
+++ Sources/PropertyAccessorClient/main.swift
@@ -11,6 +11,12 @@ struct Bar {
     @Accessor(to: \Self.foo.value)
     var fooValue: Int
     
+    @Accessor(to: \Self.foo.value)
+    var fooValue2: Int, fooValue3: Int
+    
+    @Accessor(to: \Self.foo.value)
+    let fooValueLet: Int
+    
     init(foo: Foo) {
         self.foo = foo
     }
@@ -18,5 +24,14 @@ struct Bar {
 
 var bar = Bar(foo: Foo(value: 10))
 print(bar.fooValue)
+
 bar.fooValue = 50
 print(bar.fooValue)
+
+bar.fooValue2 = 100
+print(bar.fooValue)
+
+bar.fooValue3 = 200
+print(bar.fooValue)
+
+print(bar.fooValueLet)
