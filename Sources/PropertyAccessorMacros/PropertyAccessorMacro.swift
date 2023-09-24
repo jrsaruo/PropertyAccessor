a046116032d77e7344fabd5f332087fd300817aa
Yusaku Nishi <jrsaruo@gmail.com>
2023-09-24 17:45:26 +0900

    delete: template

--- Sources/PropertyAccessorMacros/PropertyAccessorMacro.swift
+++ Sources/PropertyAccessorMacros/PropertyAccessorMacro.swift
@@ -3,28 +3,6 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
-/// Implementation of the `stringify` macro, which takes an expression
-/// of any type and produces a tuple containing the value of that expression
-/// and the source code that produced the value. For example
-///
-///     #stringify(x + y)
-///
-///  will expand to
-///
-///     (x + y, "x + y")
-public struct StringifyMacro: ExpressionMacro {
-    public static func expansion(
-        of node: some FreestandingMacroExpansionSyntax,
-        in context: some MacroExpansionContext
-    ) -> ExprSyntax {
-        guard let argument = node.argumentList.first?.expression else {
-            fatalError("compiler bug: the macro does not have any arguments")
-        }
-        
-        return "(\(argument), \(literal: argument.description))"
-    }
-}
-
 /// Implementation of the `Accessor` macro attached to the property,
 /// which adds the getter and the setter via the specified key path to the property.
 ///
@@ -69,7 +47,6 @@ public struct PropertyAccessorMacro: AccessorMacro {
 @main
 struct PropertyAccessorPlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
-        StringifyMacro.self,
         PropertyAccessorMacro.self
     ]
 }
--- Tests/PropertyAccessorTests/PropertyAccessorTests.swift
+++ Tests/PropertyAccessorTests/PropertyAccessorTests.swift
@@ -7,43 +7,11 @@ import XCTest
 import PropertyAccessorMacros
 
 let testMacros: [String: Macro.Type] = [
-    "stringify": StringifyMacro.self,
     "Accessor": PropertyAccessorMacro.self
 ]
 #endif
 
 final class PropertyAccessorTests: XCTestCase {
-    func testMacro() throws {
-        #if canImport(PropertyAccessorMacros)
-        assertMacroExpansion(
-            """
-            #stringify(a + b)
-            """,
-            expandedSource: """
-            (a + b, "a + b")
-            """,
-            macros: testMacros
-        )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
-    }
-    
-    func testMacroWithStringLiteral() throws {
-        #if canImport(PropertyAccessorMacros)
-        assertMacroExpansion(
-            #"""
-            #stringify("Hello, \(name)")
-            """#,
-            expandedSource: #"""
-            ("Hello, \(name)", #""Hello, \(name)""#)
-            """#,
-            macros: testMacros
-        )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
-    }
     
     func testPropertyAccessorMacro() throws {
         #if canImport(PropertyAccessorMacros)
